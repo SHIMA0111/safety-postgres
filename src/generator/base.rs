251d9e3c06a5c0c513939c3a8f04420b8ed27e20
SHIMA0111 <shima@little-tabby.com>
2024-03-30 15:59:57 +0900

    update query generator method

--- src/generator/base.rs
+++ src/generator/base.rs
@@ -1,6 +1,6 @@
 pub trait Generator {
-    fn get_statement() -> String;
-    fn get_params() -> Vec<String>;
+    fn get_statement(&self) -> String;
+    fn get_params(&self) -> Vec<String>;
 }
 
 pub enum ConditionOperator {
@@ -10,4 +10,31 @@ pub enum ConditionOperator {
     GreaterEq,
     Lower,
     LowerEq
+}
+
+impl ConditionOperator {
+    pub(crate) fn get_symbol(&self) -> String {
+        match self {
+            ConditionOperator::Equal => "=".to_string(),
+            ConditionOperator::NotEqual => "!=".to_string(),
+            ConditionOperator::Greater => ">".to_string(),
+            ConditionOperator::GreaterEq => ">=".to_string(),
+            ConditionOperator::Lower => "<".to_string(),
+            ConditionOperator::LowerEq => "<=".to_string(),
+        }
+    }
+}
+
+pub enum ChainMethod {
+    And,
+    Or
+}
+
+impl ChainMethod {
+    pub(crate) fn get_string(&self) -> String {
+        match self {
+            ChainMethod::And => "AND".to_string(),
+            ChainMethod::Or => "OR".to_string(),
+        }
+    }
 }
--- src/generator/query.rs
+++ src/generator/query.rs
@@ -1,56 +1,21 @@
 use std::collections::HashSet;
-use crate::generator::base::{ConditionOperator, Generator};
+use crate::generator::base::{ChainMethod, ConditionOperator, Generator};
 use crate::utils::errors::GeneratorError;
-use crate::utils::helpers::{Pair, Variable};
+use crate::utils::helpers::Variable;
 
 struct QueryGenerator<'a> {
     main_schema: Option<&'a str>,
     main_table: &'a str,
-    main_query_columns: &'a[&'a str],
+    main_query_columns: QueryColumns<'a>,
     join_tables: Vec<JoinTable<'a>>,
     conditions: Vec<Condition<'a>>,
     include_tables: HashSet<String>,
 }
 
-pub enum JoinTable<'a> {
-    SameSchemaTable{
-        table_name: &'a str,
-        query_columns: &'a[&'a str],
-        join_columns: Vec<Pair<&'a str>>,
-    },
-    AnotherSchemaTable {
-        schema_name: &'a str,
-        table_name: &'a str,
-        query_columns: &'a[&'a str],
-        join_columns: Vec<Pair<&'a str>>,
-    }
-}
-
-pub enum Condition<'a> {
-    OnMainTable {
-        column: &'a str,
-        value: Variable,
-        operator: ConditionOperator,
-    },
-    SameSchemaTable {
-        table_name: &'a str,
-        column: &'a str,
-        value: Variable,
-        operator: ConditionOperator,
-    },
-    AnotherSchemaTable {
-        schema_name: &'a str,
-        table_name: &'a str,
-        column: &'a str,
-        value: Variable,
-        operator: ConditionOperator,
-    }
-}
-
 impl<'a> QueryGenerator<'a> {
     pub fn new(
         main_table: &'a str,
-        query_columns: &'a [&'a str],
+        query_columns: QueryColumns<'a>,
         main_schema: Option<&'a str>) -> QueryGenerator<'a> {
         Self {
             main_schema,
@@ -83,11 +48,27 @@ impl<'a> QueryGenerator<'a> {
 }
 
 impl Generator for QueryGenerator<'_> {
-    fn get_statement() -> String {
+    fn get_statement(&self) -> String {
         todo!()
     }
-    fn get_params() -> Vec<String> {
+    fn get_params(&self) -> Vec<String> {
         todo!()
+    }
+}
+
+pub enum JoinTable<'a> {
+    SameSchemaTable{
+        table_name: &'a str,
+        query_columns: QueryColumns<'a>,
+        join_columns: JoinColumns<'a>,
+        join_method: JoinType,
+    },
+    AnotherSchemaTable {
+        schema_name: &'a str,
+        table_name: &'a str,
+        query_columns: QueryColumns<'a>,
+        join_columns: JoinColumns<'a>,
+        join_method: JoinType,
     }
 }
 
@@ -100,6 +81,104 @@ impl JoinTable<'_> {
                 schema_name,
                 table_name, ..} => format!("{}.{}", schema_name, table_name),
         }
+    }
+
+    fn get_join_type(&self) -> &JoinType {
+        match self {
+            JoinTable::AnotherSchemaTable {join_method, ..} => join_method,
+            JoinTable::SameSchemaTable {join_method, ..} => join_method,
+        }
+    }
+
+    fn get_join_columns(&self) -> &JoinColumns {
+        match self {
+            JoinTable::AnotherSchemaTable { join_columns, .. } => join_columns,
+            JoinTable::SameSchemaTable { join_columns, .. } => join_columns,
+        }
+    }
+
+    fn get_join_statement(&self, main_table_name: &str) -> String {
+        let mut join_statement_vec: Vec<String> = Vec::new();
+
+        let join_method = match self.get_join_type() {
+            JoinType::Inner => "JOIN",
+            JoinType::Left => "LEFT JOIN",
+            JoinType::Right => "RIGHT JOIN",
+            JoinType::Full => "FULL JOIN"
+        };
+        join_statement_vec.push(join_method.to_string());
+
+        let table_name = self.get_table_name();
+        join_statement_vec.push(table_name.to_string());
+
+        let join_columns = match self.get_join_columns() {
+            JoinColumns::AllColumns => "TRUE".to_string(),
+            JoinColumns::SpecifyColumns(columns) => {
+                let mut join_columns_vec = Vec::new();
+                for column in columns {
+                    if join_columns_vec.len() != 0 {
+                        join_columns_vec.push(column.bind_method.get_string())
+                    }
+                    let one_join = format!(
+                        "{}.{} {} {}.{}",
+                        main_table_name, &column.main_joined_column,
+                        column.operator.get_symbol(),
+                        table_name,
+                        &column.joined_column
+                    );
+                    join_columns_vec.push(one_join);
+                }
+                join_columns_vec.join(" ")
+            }
+        };
+        join_statement_vec.push(join_columns);
+
+        join_statement_vec.join(" ")
+    }
+}
+
+pub enum JoinColumns<'a> {
+    AllColumns,
+    SpecifyColumns(Vec<JoinColumn<'a>>)
+}
+
+struct JoinColumn<'a> {
+    main_joined_column: &'a str,
+    joined_column: &'a str,
+    operator: ConditionOperator,
+    bind_method: ChainMethod
+}
+
+pub enum QueryColumns<'a> {
+    AllColumns,
+    SpecifyColumns(&'a[&'a str])
+}
+
+pub enum JoinType {
+    Inner,
+    Left,
+    Right,
+    Full,
+}
+
+pub enum Condition<'a> {
+    OnMainTable {
+        column: &'a str,
+        value: Variable,
+        operator: ConditionOperator,
+    },
+    SameSchemaTable {
+        table_name: &'a str,
+        column: &'a str,
+        value: Variable,
+        operator: ConditionOperator,
+    },
+    AnotherSchemaTable {
+        schema_name: &'a str,
+        table_name: &'a str,
+        column: &'a str,
+        value: Variable,
+        operator: ConditionOperator,
     }
 }
 
